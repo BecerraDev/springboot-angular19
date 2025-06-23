e6b2cd2d14750826ac418d7f53e5fe2c882c8c70
Byron Becerra <byron.becerra.aravena@gmail.com>
2025-06-23 11:16:01 -0400

    Convertir frontend-catalogo en carpeta normal

---
+++ frontend-catalogo/src/app/app.config.ts
@@ -0,0 +1,33 @@
+// src/app/app.config.ts
+import { ApplicationConfig } from '@angular/core';
+import { provideKeycloak, withAutoRefreshToken } from 'keycloak-angular';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    // Proveedor de Keycloak para autenticación
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8080/',
+        realm: 'miapp',
+        clientId: 'frontend'
+      },
+      // Opciones de inicialización para controlar login
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false
+      },
+      // Refresco automático del token con logout por inactividad
+      features: [
+        withAutoRefreshToken({
+          onInactivityTimeout: 'logout',
+          sessionTimeout: 60_000
+        })
+      ]
+    }),
+
+    // Proveedor del router con rutas definidas
+    provideRouter(routes)
+  ]
+};
